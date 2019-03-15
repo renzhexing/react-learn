0897b99f74dbe70bb10127d4b649f4f052e001a3
renjunfu <1044764884@qq.com>
2019-03-16 01:16:16 +0800

    add delete function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,53 @@
 import React, { Component, Fragment } from "react";
 
 class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleSubmit() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handleDelete(index) {
+    let list = [...this.state.list]
+    list.splice(index,1)
+    this.setState({
+      list: list
+    })
+  }
+
   render() {
     return (
       <Fragment>
         <div>
-          <input />
-          <button>提交</button>
+          <input 
+            value={this.state.inputValue}
+            onChange={this.handleChange.bind(this)}
+          />
+          <button onClick={this.handleSubmit.bind(this)}>提交</button>
         </div>
         <ul>
-          <li>学习英语</li>
-          <li>学习Rect</li>
+          {
+            this.state.list.map((item,index)=>{
+              return <li key={index} onClick={this.handleDelete.bind(this,index)}>{item}</li>
+            })
+          }
         </ul>
       </Fragment>
     );
