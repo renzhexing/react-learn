4e3deb151ab20faa3b9857a357da1e7a80c68c80
renjunfu <1044764884@qq.com>
2019-03-24 23:53:40 +0800

    react redux store

--- src/list.js
+++ src/list.js
@@ -7,6 +7,12 @@ class Lists extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handStoreChange = this.handStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+
+    store.subscribe(this.handStoreChange)
   }
   componentDidMount() {
     // axios
@@ -19,13 +25,39 @@ class Lists extends Component {
     //   });
     // console.log(store.getState());
   }
+
+  handleInputChange(e) {
+    console.log(e.target.value)
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    };
+    store.dispatch(action)
+  };
+
+  handStoreChange() {
+    this.setState(store.getState())
+  };
+
+  handleBtnClick() {
+    const action = {
+      type: 'add_todo_item'
+    }
+
+    store.dispatch(action);
+  }
+
   render() {
     const {list,inputValue} = this.state;
     return (
       <Fragment>
         <div style={{ display: "flex" }}>
-          <Input placeholder="todo list"  value={inputValue}/>
-          <Button type="primary">提交</Button>
+          <Input 
+          placeholder="todo list"  
+          value={inputValue}
+          onChange={this.handleInputChange}/>
+          <Button type="primary"
+          onClick = {this.handleBtnClick}>提交</Button>
         </div>
         <List
           header={<div>Header</div>}
